f63421159976ec23ec51b1cb89069257feb74a92
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 22:43:39 +0700

    init

---
+++ ModuleA/ModuleA.swift
@@ -0,0 +1,16 @@
+//
+//  ModuleA.swift
+//  ModuleA
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+import Foundation
+
+public class ModuleA {
+    public init() {}
+    
+    public func getModuleA() {
+        print("Module A")
+    }
+}
